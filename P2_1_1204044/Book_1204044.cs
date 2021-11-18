3136aa0edd14f70ea5ad25c8bcc541e9ff3f3034
Fahira <fahira1678@gmail.com>
2021-11-18 11:03:18 +0700

    Initial commit

---
+++ P2_1_1204044/Book_1204044.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_1204044
+{
+
+    public class Book_1204044 : Product_1204044
+    {
+        protected string pageCount;
+
+        public Book_1204044(string type, string title, string pagecount)
+            : base(type, title)
+        {
+            this.pageCount = pagecount;
+        }
+
+        public string PageCount
+        {
+            get
+            {
+                return pageCount;
+            }
+
+            set
+            {
+                pageCount = value;
+            }
+        }
+    }
+}
